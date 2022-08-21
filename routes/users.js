3821f97819b3b6883df0eb1cc483d4f82c087124
shiranaa <shiraaanm@gmail.com>
2022-08-21 21:27:27 +0300

    update routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,39 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
-const { User, validate } = require("../models/user");
+const { User, validate, validateCards } = require("../models/user");
+const { Card } = require("../models/card");
 const auth = require("../middleware/auth");
 const router = express.Router();
+
+const getCards = async (cardsArray) => {
+  const cards = await Card.find({ bizNumber: { $in: cardsArray } });
+  return cards;
+};
+
+router.get("/cards", auth, async (req, res) => {
+  if (!req.query.numbers) res.status(400).send("Missing numbers data");
+
+  let data = {};
+  data.cards = req.query.numbers.split(",");
+
+  const cards = await getCards(data.cards);
+  res.send(cards);
+});
+
+router.patch("/cards", auth, async (req, res) => {
+  const { error } = validateCards(req.body);
+  if (error) res.status(400).send(error.details[0].message);
+
+  const cards = await getCards(req.body.cards);
+  if (cards.length != req.body.cards.length)
+    res.status(400).send("Card numbers don't match");
+
+  let user = await User.findById(req.user._id);
+  user.cards = req.body.cards;
+  user = await user.save();
+  res.send(user);
+});
 
 router.get("/me", auth, async (req, res) => {
   const user = await User.findById(req.user._id).select("-password");
